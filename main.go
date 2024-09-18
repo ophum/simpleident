c3cd7758aa66ca9bc4ff7d9012b54d31bfc49722
ophum <inagaki0106@gmail.com>
2024-09-19 06:22:22 +0900

    basic auth test

--- main.go
+++ main.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/memstore"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 )
 
 //go:embed templates/*.tmpl templates/admin/*.tmpl
 var f embed.FS
+
+var config Config
+
+type Config struct {
+	AdminBasicAuthAccounts gin.Accounts `yaml:"adminBasicAuthAccounts"`
+}
+
+func init() {
+	configPath := flag.String("config", "config.yaml", "config.yaml")
+	flag.Parse()
+
+	f, err := os.Open(*configPath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if err := yaml.NewDecoder(f).Decode(&config); err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -30,6 +54,7 @@ func main() {
 		ctx.HTML(http.StatusOK, "index.html.tmpl", gin.H{})
 	})
 
+	AdminRegisterRoutes(r, config.AdminBasicAuthAccounts)
 	AccountRegisterRoutes(r)
 	OAuth2ClientRegisterRoutes(r)
 	if err := r.Run(); err != nil {
